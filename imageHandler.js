2764c4d53b8803c6dba393deb6533f60993f05fb
pomalone91 <pomalone91@gmail.com>
2019-03-14 20:43:31 -0600

    added max file size restriction. Moved appendChild() to after file read.

--- imageHandler.js
+++ imageHandler.js
@@ -1,3 +1,5 @@
+const MAX_FILE_SIZE = 500000; // Bytes
+const ACCEPTABLE_HW_RATIO = {lowerBound: 0.98, upperBound: 1.02};
 const bodySelector = document.getElementById('body'); // does this work?
 const fileSelect = document.getElementById("chess-image");
 fileSelect.addEventListener("change", function (e) {
@@ -7,13 +9,27 @@ fileSelect.addEventListener("change", function (e) {
 function displayFile(file) {
     const img = document.createElement("img");
     img.file = file;
-    bodySelector.appendChild(img); 
+    
     // read the file, see https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications 
     const reader = new FileReader();
     reader.onload = (function(aImg) { 
+    
         return function(e) { 
+            
+            if (aImg.file.size > MAX_FILE_SIZE || aImg.file.size == 0) {
+                return;
+            }
+
+            // Not working. height and width aren't accessible until on DOM.
+//             var hwRatio = parseInt(aImg.naturalHeight) / parseInt(aImg.naturalWidth);
+//             if (hwRatio < ACCEPTABLE_HW_RATIO.lowerBound || hwRatio > ACCEPTABLE_HW_RATIO.upperBound ) {
+//                 console.log("bad pic");
+//                 return;
+//             }
             aImg.src = e.target.result; 
+            
         }; 
     })(img);
     reader.readAsDataURL(file);
+    bodySelector.appendChild(img); 
 }
